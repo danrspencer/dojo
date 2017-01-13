853fb51e801e873be17ec436b8cb5ba65716b242
Daniel Spencer <daniel.spencer2@skybettingandgaming.com>
2017-01-13 12:14:42 +0000

    Updated the JS portion of the dojo to use more modern javascript

---
+++ javascript/spec/Sample.spec.js
@@ -0,0 +1,9 @@
+
+import { sample } from "../src/sample";
+
+describe("sample", () => {
+
+    it('returns true', function () {
+        expect(sample()).toBe(true);
+    });
+});
--- javascript/src/Sample.js
+++ javascript/src/Sample.js
@@ -1,17 +1,2 @@
-define(["require", "exports"], function(require, exports) {
 
-    var Sample = (function () {
-
-        function Sample() {
-        }
-
-        Sample.prototype.example = function () {
-            return true;
-        };
-
-        return Sample;
-    })();
-
-    
-    return Sample;
-});
+export const sample = () => true;
