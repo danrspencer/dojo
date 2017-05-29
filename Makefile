6129bb69fc86af0db14a3d792b334658bf666a1f
Dan Spencer <dan.spencer@anaplan.com>
2017-05-29 16:22:14 +0100

    Updated some watcher stuff and started on Java

--- Makefile
+++ Makefile
@@ -3,11 +3,15 @@ javascript:
 	cd languages/javascript && yarn install && yarn test -- --watch
 
 typescript:
+	@echo TODO
 
 elm:
 	cd languages/elm && yarn install && yarn elm-package install && yarn test -- --watch
 
 php:
+	@echo TODO
 
 java:
 
+intellij-file-watchers:
+	cp ./resources/intellij/watcherTasks.xml ./idea/watcherTasks.xml
--- languages/elm/tests/KataTest.elm
+++ languages/elm/tests/KataTest.elm
@@ -3,16 +3,18 @@ module KataTest exposing (..)
 import ElmTest.Extra exposing (..)
 import Expect
 
+
 ---
 
 import Kata exposing (..)
+
 
 all : Test
 all =
     describe "Sample Test Suite"
         [ describe "Unit test examples"
-            [            test "The Kata" <|
-                             \() ->
-                                 Kata.sum 1 2 |> Expect.equal 3
+            [ test "The Kata" <|
+                \() ->
+                    Kata.sum 1 2 |> Expect.equal 3
             ]
         ]
